ddf8b3528c43f281b8b19ebfb0cbbb2f14605957
Christian Budde Christensen <budde377@gmail.com>
2017-08-28 09:45:21 +0200

    feat: Add index file and Docs

---
+++ src/index.js
@@ -0,0 +1,6 @@
+// @flow
+
+export {default as Dispatcher} from './dispatcher'
+export {default as Handler} from './handler'
+export {default as Storage} from './storage/storage'
+export {default as MemoryStorage} from './storage/memory'
